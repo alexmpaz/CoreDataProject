68649f1af674735b122c638f2b6135556e2e3ceb
Alex Paz <alexmpaz@gmail.com>
2022-02-16 16:14:40 +0000

    Initial Commit

---
+++ CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by Alex Paz on 16/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
